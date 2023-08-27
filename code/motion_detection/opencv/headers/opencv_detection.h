b86f5312347aac889cd61610d344cecc9b36ed0a
TapekhaVA <tapehavladimir@gmail.com>
2023-08-27 20:10:54 +0300

    some refactoring

--- code/motion_detection/opencv/headers/opencv_detection.h
+++ code/motion_detection/opencv/headers/opencv_detection.h
@@ -12,14 +12,21 @@
 class OpenCVDetection
 {
 public:
-    explicit OpenCVDetection(int threshold = 115,
-                             cv::Size params = {200, 200})
-                             : _threshold_value(threshold),
-                               _params(std::move(params)),
+    explicit OpenCVDetection(cv::Size params = {200, 200},
+                             int threshold = 43,
+                             cv::Size dilate_kernel_size = {3, 3},
+                             size_t _frames = 15,
+                             cv::Size blur_kernel_size = {15, 15})
+                             : _params(std::move(params)),
+                               _threshold_value(threshold),
+                               _dilate_kernel_size(std::move(dilate_kernel_size)),
+                               _blur_kernel_size(std::move(blur_kernel_size)),
                                _sum_frames(cv::Mat::zeros(_params, CV_8UC3)),
-                               _capacity(20) {}
+                               _capacity(_frames) {}
 
     void detectMotion(cv::Mat& cur_frame);
+private:
+    std::vector<std::vector<cv::Point>> findContours(const cv::Mat& cur_frame);
 
     [[nodiscard]] cv::Mat getAbsDiff(const cv::Mat& cur_frame) const;
 
@@ -27,10 +34,16 @@ public:
 
     void changeSum(const cv::Mat& cur_frame);
 
+    void changeRectangles(std::vector<cv::Rect>& rectangles);
+
     [[nodiscard]] cv::Mat getMeanSum() const;
+
 private:
-    int _threshold_value;
     cv::Size _params;
+    int _threshold_value;
+
+    cv::Size _dilate_kernel_size;
+    cv::Size _blur_kernel_size;
 
     const size_t _capacity;
     cv::Mat _sum_frames;
--- code/utilities/headers/geometry.h
+++ code/utilities/headers/geometry.h
@@ -4,16 +4,18 @@
 #include <vector>
 #include <opencv4/opencv2/opencv.hpp>
 
-class Geometry
-{
-public:
-    static void mergeContours(std::vector<cv::Point>& first, const std::vector<cv::Point>& second);
-    static double checkDistContours(const std::vector<cv::Point>& first, const std::vector<cv::Point>& second);
 
-    static bool isInnerRectangle(const cv::Rect& first, const cv::Rect& second);
-private:
-    static inline bool isInnerPoint(const cv::Point& point, const cv::Rect& rect) { return point.inside(rect); }
-    static double calculateDistance(const cv::Point& p1, const cv::Point& p2);
-};
+namespace Geometry
+{
+    void mergeContours(std::vector<cv::Point> &first, const std::vector<cv::Point> &second);
+
+    double checkDistContours(const std::vector<cv::Point> &first, const std::vector<cv::Point> &second);
+
+    bool isInnerRectangle(const cv::Rect &first, const cv::Rect &second);
+
+    inline bool isInnerPoint(const cv::Point &point, const cv::Rect &rect) { return point.inside(rect); }
+
+    double calculateDistance(const cv::Point &p1, const cv::Point &p2);
+}
 
 #endif //VIDEO_ANALYTICS_DETECTORS_GEOMETRY_H
